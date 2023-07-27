c24be9afe7ece33fb0ec2933eeae33c6d876a0d5
Deolu_Josef <kiishijoseph@gmail.com>
2023-07-27 13:47:35 +0100

    inital commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        data = request.get_json()
+    else:
+        data = request.form
+    
+    bedrooms = data['bedrooms']
+    bathrooms = data['bathrooms']
+    floors = data['floors']
+    yr_built = data['yr_built']
+    arr = np.array([bedrooms, bathrooms,floors, yr_built])
+    arr = arr.astype(np.float64)
+    pred = model.predict([arr])
+
+    # return render_template('index.html', data=int(pred))
+    return jsonify({'data':int(pred)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
